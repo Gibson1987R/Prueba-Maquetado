2486c2b3a1be705dacca667e13bfdc2144116d1d
Gibson Rosales <gibsonrosales@gmail.com>
2020-11-29 08:44:16 -0400

    :3era version

--- src/components/Reproductor.jsx
+++ src/components/Reproductor.jsx
@@ -1,22 +1,11 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
 import Fondo from '../images/Momentos.mp4';
-import './styles/styles.css';
 class Reproductor extends React.Component {
 
   render() {
     return (
       <div className='contenedor'
-      // style={{
-      //   width :'100%', 
-      //   height:'100%', 
-      //   position:'absolute', 
-      //   zIndex: '-1',
-      //   top:'50%',
-      //   left:'50%',
-      //   objectFit:'cover',
-      //   transform: 'translate(-50%,-50%)',
-      // }}
       >
         <ReactPlayer
           url = {Fondo}
